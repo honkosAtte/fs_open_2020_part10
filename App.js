2bd3f9b493d199529118459bbf06e6ffbcc81a6a
Atte Honkonen <atte.honkonen@gmail.com>
2020-12-15 11:29:19 +0200

    10.3

--- App.js
+++ App.js
@@ -1,12 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View , TouchableWithoutFeedback, Alert} from 'react-native';
+import RepositoryList from './components/RepositoryList';
 
 export default function App() {
+
+console.log('terve');
   return (
     <View style={styles.container}>
-     <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+     {/* <Text>Open up App.js to stop doing  on your app!</Text>
+     <TouchableWithoutFeedback
+      onPress={() => Alert.alert('You pressed the text!')}
+
+    >
+      <Text>You can press me</Text>
+ 
+    </TouchableWithoutFeedback>
+      <StatusBar style="auto" /> */}
+      <RepositoryList/>
     </View>
   );
 }
